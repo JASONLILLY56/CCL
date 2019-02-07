0aa5b0bc45e495922572558e743c707fade7e8e4
Jason <jason@JasondeMacBook-Pro.local>
2019-02-07 23:25:34 +0800

    first commit

---
+++ 1.4_2.cc
@@ -0,0 +1,19 @@
+///
+///@file     1.4_2.cc
+///@author     Jason(13933536151@163.com)
+///@data     2019-02-07 23:02:1549551772
+///
+  
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+ 
+int main()
+{
+    int i = 10,*p=&i;
+  cout<<"p="<<p<<endl;
+ cout<<"*p="<<*p<<endl;
+cout<<"&i="<<&i<<endl;
+return 0;
+}
